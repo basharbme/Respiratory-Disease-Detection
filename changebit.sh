a6abd5ff0969c1c3b4bd96cfb3e4d287d4bf22d8
pnshinde <pnshinde@umich.edu>
2020-04-01 13:21:23 -0400

    got spectrograms and corresponding files

--- changebit.sh
+++ changebit.sh
@@ -6,6 +6,6 @@ for f in $path
 do
     rep="_16.wav"
     tempFile="${f/.wav/$rep}"
-    sox -v 0.99 $f -b 16 $tempFile
+    sox -v 0.99 $f -b 16 -r 4000 $tempFile
     echo "Successfully converted $f to 16 bit depth"
 done
