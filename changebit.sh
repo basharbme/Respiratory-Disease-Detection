b9ab6c4ac31eae5d259ee6a3ea53c4cacf471e64
pnshinde <pnshinde@umich.edu>
2020-04-06 17:06:56 -0400

    deleted all files with sampling rate 4000, converted remaining files to 8000

--- changebit.sh
+++ changebit.sh
@@ -6,6 +6,6 @@ for f in $path
 do
     rep="_16.wav"
     tempFile="${f/.wav/$rep}"
-    sox -v 0.99 $f -b 16 -r 4000 $tempFile
-    echo "Successfully converted $f to 16 bit depth"
+    sox -v 0.99 $f -b 16 -r 8000 $tempFile
+    echo "Successfully converted $f to 16 bit depth, 0.99 volume and 8000 sampling rate."
 done
